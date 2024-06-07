826bdcee23c8c7926df16b2f31183563bba4e1a1
unknown <bhoure21@gmail.com>
2024-06-07 15:38:43 +0530

    building product page and also adding the manage prodcut as well as add product funtionality

---
+++ src/pages/Managements/ProductManagement.tsx
@@ -0,0 +1,56 @@
+import ProductForm from "../../components/ProductForm"
+import Sidebar from "../../components/Sidebar"
+import { useState } from "react"
+
+interface FormDataType {
+  name: string;
+  price: number;
+  stock: number;
+  image: string;
+}
+
+function ProductManagement() {
+
+  const [productDetails, setProductDetails] = useState<FormDataType>({
+    name: 'Nike',
+    price: 2,
+    stock: 3,
+    image: ''
+  })
+
+
+  
+  return (
+    <div className="grid grid-cols-5 gap-8 h-[100vh] bg-slate-200">
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* /Main */}
+      <main className="col-span-4 flex justify-center items-center gap-4">
+
+        <section className="overflow-y-auto w-full h-[85vh] max-w-[500px] shadow-md bg-white p-20 flex flex-col gap-4 relative rounded">
+
+          <strong className="font-300">Product Id - afnkjafkd</strong>
+
+          <img className="w-full h-full object-cover" src={""} alt={""} />
+
+          <p className="text-center tracking-wide uppercase ">{productDetails.name}</p>
+
+          {productDetails.stock > 0 ? (<span className="absolute right-8 top-8 text-green-600">{productDetails.stock} Availables</span>) : (<span  className="absolute right-8 top-8 text-red-500">Not Availvable</span>)}
+
+          <h3 className="text-center text-2xl">{productDetails.price}</h3>
+
+        </section>
+
+        <section className="h-[85vh] p-8 w-full max-w-[400px] bg-white rounded-md shadow-md">
+          <ProductForm data={productDetails} />
+        </section>
+
+      </main>
+
+    </div>
+  )
+}
+
+export default ProductManagement
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,16 +1,118 @@
+import { ReactElement, useCallback, useState } from "react";
 import Sidebar from "../components/Sidebar"
-const Products = () => {
-  return (
-    <div className="grid grid-cols-5 h-[100vh] bg-slate-200">
-       
-    {/* Sidebar */}
-    <Sidebar />
+import TableHOC from "../components/TableHOC";
+import { Column } from "react-table";
+import { Link } from "react-router-dom";
+import { FaPlus } from "react-icons/fa";
 
-    {/* /Main */}
-    <main className="col-span-4"> 
-     products
-    </main>
- </div>
+interface DataType {
+  photo: ReactElement;
+  name: string;
+  price: number;
+  stock: number;
+  action: ReactElement;
+}
+
+const columns: Column<DataType>[] = [
+  {
+    Header: "Photo",
+    accessor: "photo",
+  },
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Price",
+    accessor: "price",
+  },
+  {
+    Header: "Stock",
+    accessor: "stock",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  }
+]
+
+const img =
+  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
+
+const img2 = "https://m.media-amazon.com/images/I/514T0SvwkHL._SL1500_.jpg";
+
+const arr: DataType[] = [
+  {
+    photo: <img src={img} alt="Shoes" />,
+    name: "Puma Shoes Air Jordan Cook Nigga 2023",
+    price: 690,
+    stock: 3,
+    action: <Link to="/admin/product/sajknaskd">Manage</Link>,
+  },
+
+  {
+    photo: <img src={img2} alt="Shoes" />,
+    name: "Macbook",
+    price: 232223,
+    stock: 213,
+    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
+  },
+  {
+    photo: <img src={img} alt="Shoes" />,
+    name: "Puma Shoes Air Jordan Cook Nigga 2023",
+    price: 690,
+    stock: 3,
+    action: <Link to="/admin/product/sajknaskd">Manage</Link>,
+  },
+
+  {
+    photo: <img src={img2} alt="Shoes" />,
+    name: "Macbook",
+    price: 232223,
+    stock: 213,
+    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
+  },
+  {
+    photo: <img src={img} alt="Shoes" />,
+    name: "Puma Shoes Air Jordan Cook Nigga 2023",
+    price: 690,
+    stock: 3,
+    action: <Link to="/admin/product/sajknaskd">Manage</Link>,
+  },
+
+  {
+    photo: <img src={img2} alt="Shoes" />,
+    name: "Macbook",
+    price: 232223,
+    stock: 213,
+    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
+  },
+  {
+    photo: <img src={img2} alt="Shoes" />,
+    name: "Macbook",
+    price: 232223,
+    stock: 213,
+    action: <Link to="/admin/product/sdaskdnkasjdn">Manage</Link>,
+  },
+];
+
+const Products = () => {
+
+  const [data, setData] = useState<DataType[]>(arr)
+  const Table = useCallback(TableHOC<DataType>(columns, data, "product-box", "All Product", true), []);
+
+  return (
+    <div className="grid grid-cols-5 gap-8 h-[100vh] bg-slate-200">
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* /Main */}
+      <main className="col-span-4 ">
+        {Table()}
+        <Link to="/admin/product/new" className="fixed right-8 top-8 h-10 w-10 flex justify-center items-center rounded-[50%] bg-red-600 text-white hover:opacity-80" > <FaPlus /> </Link>
+      </main>
+    </div>
   )
 }
 
