e97d9543bf832ad458f203403b9114074eb7c37b
unknown <bhoure21@gmail.com>
2024-06-07 15:35:17 +0530

    updating table for sorting and pagination

--- src/components/TableHOC.tsx
+++ src/components/TableHOC.tsx
@@ -1,4 +1,6 @@
-import { useTable, Column, TableOptions } from "react-table"
+import { useTable, Column, TableOptions, useSortBy, usePagination } from "react-table"
+import '../style/product_css.css'
+import '../style/table.css'
 
 // type temp = {
 //     status: string,
@@ -6,48 +8,74 @@ import { useTable, Column, TableOptions } from "react-table"
 //     amount: number,
 // }
 
-function TableHOC <T extends Object> (columns: Column<T>[], data: T[], containerClassName: string, heading: string) {
-  return function HOC(){
-    
-    const options: TableOptions<T> = {
-        columns,
-        data,
-    }
+function TableHOC<T extends Object>(columns: Column<T>[], data: T[], containerClassName: string, heading: string, showPagination: boolean = false) {
+    return function HOC() {
 
-    const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable(options)
+        const options: TableOptions<T> = {
+            columns,
+            data,
+            initialState: {
+                pageSize:6,
+            },
+        }
 
-    return <div className={containerClassName}>
-          <h2>{heading}</h2>
+        const { 
+            getTableProps,
+            getTableBodyProps, 
+            headerGroups, page, 
+            prepareRow,
+            nextPage,
+            previousPage,
+            canNextPage,
+            canPreviousPage,
+            pageCount,
+            state:{pageIndex}
 
-          <table {...getTableProps()}>
-            <thead>
-                {headerGroups.map(headerGroup => (
-                    <tr {...headerGroup.getHeaderGroupProps()}>
-                        {
-                            headerGroup.headers.map(column => (
-                                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                            ))
-                        }
-                    </tr>
-                )) }
-            </thead>
-            <tbody {...getTableBodyProps()}>
-                {
-                    rows.map(row => {
-                        prepareRow(row);
-                        return <tr {...row.getRowProps()}>
+        } = useTable(options, useSortBy, usePagination)
+
+        return <div className={containerClassName}>
+            <h2 className="text-xl font-bold p-2">{heading}</h2>
+
+            <table {...getTableProps()} className="table">
+                <thead>
+                    {headerGroups.map(headerGroup => (
+                        <tr {...headerGroup.getHeaderGroupProps()}>
                             {
-                                row.cells.map(cell => {
-                                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                                })
+                                headerGroup.headers.map((column: any) => (
+                                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                                        {column.render('Header')}
+                                        {column.isSorted && <span className="text-sm">{column.isSortedDesc ? "⬇️" : "⬆️" }</span>}
+                                    </th>
+                                ))
                             }
                         </tr>
-  })
-                }
-            </tbody>
-          </table>
-    </div>
-  }
+                    ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                    {
+                        page.map(row => {
+                            prepareRow(row);
+                            return <tr {...row.getRowProps()}>
+                                {
+                                    row.cells.map(cell => {
+                                        return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                                    })
+                                }
+                            </tr>
+                        })
+                    }
+                </tbody>
+            </table>
+            
+            {
+                showPagination && data.length > 0 && <div className="pagination flex gap-4 m-8 justify-center items-center">
+                    <button  disabled={!canPreviousPage} onClick={previousPage}>⬅️</button>
+                    <span>{`${pageIndex + 1 } of ${pageCount}`}</span>
+                    <button disabled={!canNextPage} onClick={nextPage}>➡️</button>
+                </div>
+            }
+        </div>
+    }
 }
 
 export default TableHOC
