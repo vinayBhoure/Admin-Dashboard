5dd04bdfd0edd30e95e07343136fd519cf1e9907
Vinay Bhoure <92016717+vinayBhoure@users.noreply.github.com>
2024-06-08 22:08:42 +0530

    checking chrome extension

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -2,12 +2,9 @@ import { Bar } from "react-chartjs-2"
 
 const BarChart = () => {
   return (
-    <Bar data={{labels:["A", "B", "C", "D", "E", "F"],
-      datasets: [{
-        label: 'Revenue',
-        data: [12, 19, 3, 5, 2, 3],
-      }],
-    }}/>
+    <div>
+      charts
+    </div>
   )
 }
 
