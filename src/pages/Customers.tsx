1d87adcb64c853786770c3f62e5b4026c1cb2215
unknown <bhoure21@gmail.com>
2024-06-09 12:35:53 +0530

    Customer and transaction page

--- src/pages/Customers.tsx
+++ src/pages/Customers.tsx
@@ -1,17 +1,109 @@
 
+import { ReactElement, useState } from "react"
 import Sidebar from "../components/Sidebar"
-function Customers() {
-  return (
-    <div className="grid grid-cols-5 h-[100vh] bg-slate-200">
-       
-    {/* Sidebar */}
-    <Sidebar />
+import { Column } from "react-table";
+import { useCallback } from "react";
+import TableHOC from "../components/TableHOC";
+import { FaTrash } from "react-icons/fa";
 
-    {/* /Main */}
-    <main className="col-span-4"> 
-     Customer
-    </main>
- </div>
+interface DataType {
+  avatar: ReactElement,
+  name: string,
+  gender: string,
+  email: string,
+  role: string,
+  action: ReactElement
+}
+
+const columns: Column<DataType>[] = [{
+  Header: "Avatar",
+  accessor: "avatar"
+},
+{
+  Header: "Name",
+  accessor: "name"
+},
+{
+  Header: "Gender",
+  accessor: "gender"
+},
+{
+  Header: "Email",
+  accessor: "email"
+},
+{
+  Header: "Role",
+  accessor: "role"
+},
+{
+  Header: "Action",
+  accessor: "action"
+}
+
+]
+
+const img = "https://randomuser.me/api/portraits/women/54.jpg";
+const img2 = "https://randomuser.me/api/portraits/women/50.jpg";
+
+const arr: DataType[] = [
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img}
+        alt="Shoes"
+      />
+    ),
+    name: "Emily Palmer",
+    email: "emily.palmer@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+
+  {
+    avatar: (
+      <img
+        style={{
+          borderRadius: "50%",
+        }}
+        src={img2}
+        alt="Shoes"
+      />
+    ),
+    name: "May Scoot",
+    email: "aunt.may@example.com",
+    gender: "female",
+    role: "user",
+    action: (
+      <button>
+        <FaTrash />
+      </button>
+    ),
+  },
+];
+
+function Customers() {
+
+  const [data, setData] = useState<DataType[]>(arr);
+  const Table = useCallback(TableHOC<DataType>(columns, data, "customer-box", "Customers", true), []);
+  return (
+    <div className="grid grid-cols-5 gap-8 h-[100vh] bg-slate-200 p-1">
+
+      {/* Sidebar */}
+      <Sidebar />
+
+      {/* /Main */}
+      <main className="col-span-4">
+        {Table()}
+      </main>
+    </div>
   )
 }
 
--- src/pages/Transactions.tsx
+++ src/pages/Transactions.tsx
@@ -1,17 +1,89 @@
 
+import { ReactElement, useCallback, useState } from "react"
 import Sidebar from "../components/Sidebar"
-function Transactions() {
-  return (
-    <div className="grid grid-cols-5 h-[100vh] bg-slate-200">
-       
-    {/* Sidebar */}
-    <Sidebar />
+import { Column } from "react-table";
+import TableHOC from "../components/TableHOC";
+import { Link } from "react-router-dom";
 
-    {/* /Main */}
-    <main className="col-span-4"> 
-     Transacation
-    </main>
- </div>
+
+interface Datatype {
+  user: string,
+  quantity: number,
+  discount: number,
+  amount: number,
+  status: ReactElement,
+  action:ReactElement
+}
+
+const columns: Column<Datatype>[] = [{
+  Header: "User",
+  accessor: "user",
+},
+{
+  Header: "Quantity",
+  accessor: "quantity",
+},
+{
+  Header: "Discount",
+  accessor: "discount",
+},
+{
+  Header: "Amount",
+  accessor: "amount",
+},
+{
+  Header: "Status",
+  accessor: "status",
+},
+{
+  Header: "Action",
+  accessor: "action"
+}
+]
+
+const arr: Datatype[] = [
+  {
+    user: "Charas",
+    amount: 4500,
+    discount: 400,
+    quantity: 3,
+    status: <span className="red">Processing</span>,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 6999,
+    discount: 400,
+    status: <span className="green">Shipped</span>,
+    quantity: 6,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+  {
+    user: "Xavirors",
+    amount: 6999,
+    discount: 400,
+    status: <span className="purple">Delivered</span>,
+    quantity: 6,
+    action: <Link to="/admin/transaction/sajknaskd">Manage</Link>,
+  },
+];
+
+
+function Transactions() {
+
+  const [data] = useState<Datatype[]>(arr)
+  const Table = useCallback(TableHOC(columns, data, "transaction-box", "Transactions", true),[])
+  return (
+    <div className="grid grid-cols-5 gap-8 h-[100vh] bg-slate-200">
+
+      {/* Sidebar */} 
+      <Sidebar />
+
+      {/* /Main */}
+      <main className="col-span-4">
+        {Table()}
+      </main>
+    </div>
   )
 }
 
